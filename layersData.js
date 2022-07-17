87f093ea07cf390f8bbec7f176e1aca2898ba7c2
Paulina-N <78307808+Paulina-N@users.noreply.github.com>
2022-07-17 13:44:39 +0200

    panes

--- layersData.js
+++ layersData.js
@@ -46,252 +46,252 @@ var Declaration_of_Urgent_Measures = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Declaration of Urgent Measures',
     format: 'image/png',
     transparent: true,
-    //zIndex: 195
+    pane: 'Declaration_of_Urgent _Measures'
 });
 
 var Declaration_of_at_risk = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Declaration of at risk of not achieving good quantitative or chemical status',
     format: 'image/png',
     transparent: true,
-    //zIndex: 194
+    pane: 'Declaration_of_at_risk'
 });
 
 var Environmental_zoning_for_renewable_energy = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Environmental zoning for renewable energy',
     format: 'image/png',
     transparent: true,
-    //zIndex: 193
+    pane: 'Environmental_zoning_for_renewable_energy'
 });
 
 var Fartet_recovery_plan = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Fartet recovery plan',
     format: 'image/png',
     transparent: true,
-    //zIndex: 192
+    pane: 'Fartet_recovery_plan'
 });
 
 var Fishing_company_zones = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Fishing company zones',
     format: 'image/png',
     transparent: true,
-    //zIndex: 191
+    pane: 'Fishing_company_zones'
 });
 
 var Flora_recovery_plan = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Flora recovery plan',
     format: 'image/png',
     transparent: true,
-    //zIndex: 190
+    pane: 'Flora_recovery_plan'
 });
 
 var Flow_accumulation = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Flow accumulation',
     format: 'image/png',
     transparent: true,
-    //zIndex: 189
+    pane: 'Flow_accumulation'
 });
 
 var Flow_direction = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Flow direction',
     format: 'image/png',
     transparent: true,
-    //zIndex: 188
+    pane: 'Flow_direction'
 });
 
 var Irrigated_crops = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Irrigated crops',
     format: 'image/png',
     transparent: true,
-    //zIndex: 187
+    pane: 'Irrigated_crops'
 });
 
 var Macrophytes = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Macrophytes',
     format: 'image/png',
     transparent: true,
-    //zIndex: 186
+    pane: 'Macrophytes'
 });
 
 var Mar_Menor_catchment_area = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Mar Menor catchment area',
     format: 'image/png',
     transparent: true,
-    //zIndex: 185
+    pane: 'Mar_Menor_catchment_area'
 });
 
 var Marine_habitats_2018 = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Marine habitats 2018',
     format: 'image/png',
     transparent: true,
-    //zIndex: 184
+    pane: 'Marine_habitats_2018'
 });
 
 var Marine_habitats = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Marine_habitats',
     format: 'image/png',
     transparent: true,
-    //zIndex: 183
+    pane: 'Marine_habitats'
 });
 
 var Municipalities = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Municipalities',
     format: 'image/png',
     transparent: true,
-    //zIndex: 182
+    pane: 'Municipalities'
 });
 
 var National_census_dischrages = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'National_census_dischrages',
     format: 'image/png',
     transparent: true,
-    //zIndex: 181
+    pane: 'National_census_dischrages'
 });
 
 var Natura_2000_Network_LIC_ZEC = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Natura 2000 Network LIC ZEC',
     format: 'image/png',
     transparent: true,
-    //zIndex: 100
+    pane: 'Natura_2000_Network_LIC_ZEC'
 });
 
 var Natura_2000_Network_ZEPA = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Natura 2000 Network ZEPA',
     format: 'image/png',
     transparent: true,
-    //zIndex: 180
+    pane: 'Natura_2000_Network_ZEPA'
 });
 
 var Natural_protected_areas = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Natural protected areas',
     format: 'image/png',
     transparent: true,
-    //zIndex: 179
+    pane: 'Natural_protected_areas'
 });
 
 var RAMSAR = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'RAMSAR',
     format: 'image/png',
     transparent: true,
-    //zIndex: 178
+    pane: 'RAMSAR'
 });
 
 var Regions = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Regions',
     format: 'image/png',
     transparent: true,
-    //zIndex: 177
+    pane: 'Regions'
 });
 
 var Relief = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Relief',
     format: 'image/png',
     transparent: true,
-    //zIndex: 176
+    pane: 'Relief'
 });
 
 var SIAM_weather_stations = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'SIAM weather stations',
     format: 'image/png',
     transparent: true,
-    //zIndex: 175
+    pane: 'SIAM_weather_stations'
 });
 
 var SIOSE_Land_cover = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'SIOSE Land cover',
     format: 'image/png',
     transparent: true,
-    //zIndex: 174
+    pane: 'SIOSE_Land_cover'
 });
 
 var SIOSE_Land_uses = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'SIOSE Land uses',
     format: 'image/png',
     transparent: true,
-    //zIndex: 173
+    pane: 'SIOSE_Land_uses'
 });
 
 var Sailing_speed = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Sailing speed',
     format: 'image/png',
     transparent: true,
-    //zIndex: 172
+    pane: 'Sailing_speed'
 });
 
 var Selected_areas_for_the_implementation_of_thermo_solar_plants = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Selected areas for the implementation of thermo solar plants',
     format: 'image/png',
     transparent: true,
-    //zIndex: 100
+    pane: 'Selected_areas_for_the_implementation_of_thermo_solar_plants'
 });
 
 var Soil_regional_layer = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Soil regional layer',
     format: 'image/png',
     transparent: true,
-    //zIndex: 171
+    pane: 'Soil_regional_layer'
 });
 
 var Surface_water_quality = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Surface water quality',
     format: 'image/png',
     transparent: true,
-    //zIndex: 170
+    pane: 'Surface_water_quality'
 });
 
 var Temporary_exclusion_area = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Temporary exclusion area',
     format: 'image/png',
     transparent: true,
-    //zIndex: 169
+    pane: 'Temporary_exclusion_area'
 });
 
 var Terrestrial_habitats = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Terrestrial habitats',
     format: 'image/png',
     transparent: true,
-    //zIndex: 168
+    pane: 'Terrestrial_habitats'
 });
 
 var ZEPIM = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'ZEPIM',
     format: 'image/png',
     transparent: true,
-    //zIndex: 167
+    pane: 'ZEPIM'
 });
 
 var Zone_0 = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Zone 0',
     format: 'image/png',
     transparent: true,
-    //zIndex: 166
+    pane: 'Zone_0'
 });
 
 var Zone_A1 = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Zone A1',
     format: 'image/png',
     transparent: true,
-    //zIndex: 165
+    pane: 'Zone_A1'
 });
 
 var Zone_A2 = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Zone A2',
     format: 'image/png',
     transparent: true,
-    //zIndex: 164
+    pane: 'Zone_A2'
 });
 
 var Zone_B = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Zone B',
     format: 'image/png',
     transparent: true,
-    //zIndex: 163
+    pane: 'Zone_B'
 });
 
 var Zones = L.tileLayer.betterWms(smartlagoonLink, {
     layers: 'Zones',
     format: 'image/png',
     transparent: true,
-    //zIndex: 162
+    pane: 'Zones'
 });
 
 //amanda:AMANDA HTTP404 !!! error
@@ -300,19 +300,19 @@ var DEM_AMSS = L.tileLayer.betterWms('https://carto.vielca.com/geoserver/amanda/
     layers: 'DEM AMSS',
     format: 'image/png',
     transparent: true,
-    //zIndex: 161
+    pane: 'DEM_AMSS'
 });
 
 var CHL = L.tileLayer.betterWms('https://carto.vielca.com/geoserver/mar_menor/wms?', {
     layers: 'CHL',
     format: 'image/png',
     transparent: true,
-    //zIndex: 160
+    pane: 'CHL'
 });
 
 var Corine_Land_Cover = L.tileLayer.betterWms('https://carto.vielca.com/geoserver/mar_menor/wms?', {
     layers: 'Corine Land Cover',
     format: 'image/png',
     transparent: true,
-    //zIndex: 159
+    pane: 'Corine_Land_Cover'
 });
